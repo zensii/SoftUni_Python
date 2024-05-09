921620295450ef153142af0023e116559a3119d2
Svetlin Ivanov <167436978+zensii@users.noreply.github.com>
2024-05-09 11:27:31 +0300

    Delete For_Loops directory

--- For_Loops/even_power_2.py
+++
@@ -1,6 +0,0 @@
-n = int(input())
-
-for num in range(n+1):
-    if num % 2 == 0:
-        num = 2 ** num
-        print(num)
--- For_Loops/sum_numbers.py
+++
@@ -1,7 +0,0 @@
-numbers = int(input())
-sum = 0
-
-for _ in range(numbers):
-    number = int(input())
-    sum += number
-print(sum)
