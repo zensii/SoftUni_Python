5a770d6f1aa2eaa8cee656f7bafbb7886ed48df0
Svetlin <167436978+zensii@users.noreply.github.com>
2024-05-22 09:24:39 +0300

    update some code

--- Python_Fundamentals/Basic_Syntax,Conditional_Statements,Loops/Exercise/easter_bread.py
+++ Python_Fundamentals/Basic_Syntax,Conditional_Statements,Loops/Exercise/easter_bread.py
@@ -2,21 +2,15 @@ budget = float(input())
 flour_price = float(input())
 eggs_price = 0.75 * flour_price
 milk_price = 1.25 * flour_price / 4
-milk_uses = 0
+price_per_bread = eggs_price + flour_price + milk_price
 breads = 0
 color_eggs = 0
 
-while True:
-    price_per_bread = eggs_price + flour_price + milk_price
+while budget >= price_per_bread:
+    budget -= price_per_bread
+    breads += 1
 
-    if budget - price_per_bread > 0:
-
-        breads += 1
-        budget -= price_per_bread
-        color_eggs += 3
-
-    else:
-        break
+    color_eggs += 3
 
     if breads % 3 == 0:
         color_eggs -= breads - 2
