93b17681317fbc648cf61d67225ee72a2c2c55d3
Svetlin <167436978+zensii@users.noreply.github.com>
2024-05-24 10:16:10 +0300

    update some solutions

--- Python_Fundamentals/Lists_basics/Exercise/number_beggars(round-robin).py
+++ Python_Fundamentals/Lists_basics/Exercise/number_beggars(round-robin).py
@@ -3,7 +3,10 @@ beggars = int(input())
 beggar_earnings = [0] * beggars
 
 for index, coin in enumerate(string_coins):
-    beggar_index = index % beggars
-    beggar_earnings[beggar_index] += int(coin)
+
+    # beggar_index = index % beggars
+    # beggar_earnings[beggar_index] += int(coin)
+
+    beggar_earnings[index % beggars] += int(coin)  # indexing directly the beggar_earnings list
 
 print(beggar_earnings)
--- Python_Fundamentals/Lists_basics/Exercise/number_beggars.py
+++ Python_Fundamentals/Lists_basics/Exercise/number_beggars.py
@@ -2,9 +2,17 @@ string_of_coins = input().split(', ')
 beggars = list(range(int(input())))
 money = [0] * len(beggars)
 
-for index in range(0, len(string_of_coins), len(beggars)):
-    for beggar in beggars:
-        if index+beggar < len(string_of_coins):
-            money[beggar] += int(string_of_coins[index+beggar])
+# for index in range(0, len(string_of_coins), len(beggars)):
+#     for beggar in beggars:
+#         if index+beggar < len(string_of_coins):
+#             money[beggar] += int(string_of_coins[index+beggar])
+
+
+# reversing the nested structure
+for beggar in beggars:
+
+    for index in range(0, len(string_of_coins)-beggar, len(beggars)):
+
+        money[beggar] += int(string_of_coins[index+beggar])
 
 print(money)
