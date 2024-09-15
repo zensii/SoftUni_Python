c42a55cb90d9f1998170bd7e5d01fe9c50ce2f26
svetl <svetlin.sii@gmail.com>
2024-09-15 09:19:55 +0300

    added a problem solution

---
+++ Python_Advanced/Stacks_and_Queues/Exercise/stacked_queues.py
@@ -0,0 +1,23 @@
+stack = []
+
+lines = int(input())
+
+for line in range(lines):
+    command = input()
+
+    if len(command.split()) == 2:
+        stack.append(int(command.split()[1]))
+    else:
+        if stack:
+            match command:
+                case '2':
+                    stack.pop()
+                case '3':
+                    print(max(stack))
+                case '4':
+                    print(min(stack))
+r_stack = []
+while stack:
+    r_stack.append(str(stack.pop()))
+
+print(', '.join(r_stack))
