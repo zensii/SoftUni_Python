91a2f426bd577aff1f364ec9c860548a9e8d1159
svetl <svetlin.sii@gmail.com>
2024-09-15 12:58:36 +0300

    added an alternative solution

--- Python_Advanced/Stacks_and_Queues/Exercise/fast_food.py
+++ Python_Advanced/Stacks_and_Queues/Exercise/fast_food.py
@@ -4,12 +4,15 @@ food = int(input())
 customers = deque(map(int, input().split()))
 max_order = max(customers)
 
-for customer in customers.copy():
-    if customer <= food:
-        food -= customer
-        customers.popleft()
-    else:
-        break
+# for customer in customers.copy():
+#     if customer <= food:
+#         food -= customer
+#         customers.popleft()
+#     else:
+#         break
+
+while customers and customers[0] <= food:
+    food -= customers.popleft()
 
 print(max_order)
-print("Orders complete" if not customers else f"Orders left: {' '.join([str(customer) for customer in customers])}")
+print("Orders complete" if not customers else f"Orders left: ",*customers)
