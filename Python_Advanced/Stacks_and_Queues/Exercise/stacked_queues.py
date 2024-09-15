506100c454c62aac7b0d393ce6773bed412b6ed8
svetl <svetlin.sii@gmail.com>
2024-09-15 12:16:43 +0300

    added an alternative solution

--- Python_Advanced/Stacks_and_Queues/Exercise/stacked_queues.py
+++ Python_Advanced/Stacks_and_Queues/Exercise/stacked_queues.py
@@ -1,22 +1,37 @@
 stack = []
 lines = int(input())
 
+# for line in range(lines):
+#     command = input()
+#
+#     if len(command.split()) == 2:
+#         stack.append(int(command.split()[1]))
+#     else:
+#         if stack:
+#             match command:
+#                 case '2':
+#                     stack.pop()
+#                 case '3':
+#                     print(max(stack))
+#                 case '4':
+#                     print(min(stack))
+# r_stack = []
+# while stack:
+#     r_stack.append(str(stack.pop()))
+#
+# print(', '.join(r_stack))
+
+# alternative advanced solution:
+
+mapper = {
+    '1': lambda x: stack.append(int(x)),
+    '2': lambda: stack.pop() if stack else None,
+    '3': lambda: print(max(stack)) if stack else None,
+    '4': lambda: print(min(stack)) if stack else None
+}
+
 for line in range(lines):
-    command = input()
+    command = input().split()
+    mapper[command[0]](*command[1:])
 
-    if len(command.split()) == 2:
-        stack.append(int(command.split()[1]))
-    else:
-        if stack:
-            match command:
-                case '2':
-                    stack.pop()
-                case '3':
-                    print(max(stack))
-                case '4':
-                    print(min(stack))
-r_stack = []
-while stack:
-    r_stack.append(str(stack.pop()))
-
-print(', '.join(r_stack))
+print(*reversed(stack), sep=', ')
