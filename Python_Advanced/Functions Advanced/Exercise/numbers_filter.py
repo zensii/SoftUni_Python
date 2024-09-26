8e529f6a9a6bdc96ec66dbebdcaf90645e70deb8
svetl <svetlin.sii@gmail.com>
2024-09-26 09:21:06 +0300

    added a problem solution

--- Python_Advanced/Functions Advanced/Exercise/numbers_filter.py
+++ Python_Advanced/Functions Advanced/Exercise/numbers_filter.py
@@ -1,11 +1,11 @@
-def even_odd_filter(even=None , odd=None ):
-    my_dict = {}
-    if even:
-        my_dict['even'] = [even_num for even_num in even if even_num % 2 == 0]
-    if odd:
-        my_dict['odd'] = [odd_num for odd_num in odd if odd_num % 2 != 0]
+def even_odd_filter(**kwargs):
 
-    return dict(sorted(my_dict.items() , key= lambda x: -len(x[1])))
+    if 'even' in kwargs:
+        kwargs['even'] = [even_num for even_num in kwargs['even'] if even_num % 2 == 0]
+    if 'odd' in kwargs:
+        kwargs['odd'] = [odd_num for odd_num in kwargs['odd'] if odd_num % 2 != 0]
+
+    return dict(sorted(kwargs.items() , key= lambda x: -len(x[1])))
 
 print(even_odd_filter(
     odd=[1, 2, 3, 4, 10, 5],
