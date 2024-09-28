e900016b993925e4e9d1a52a1cb7ee02e4a87f3c
svetl <svetlin.sii@gmail.com>
2024-09-28 22:45:35 +0300

    added a problem solution

---
+++ Python_Advanced/Error Handling/Exercise/email_validator.py
@@ -0,0 +1,26 @@
+class NameTooShortError(Exception):
+    """Name must be more than 4 characters"""
+    pass
+
+class MustContainAtSymbolError(Exception):
+    """Email must contain @"""
+    pass
+
+class InvalidDomainError(Exception):
+    """Domain must be one of the following: .com, .bg, .org, .net"""
+    pass
+
+while True:
+    email = input()
+    if email == 'End':
+        break
+
+    name = email.split('@')[0]
+    if len(name) <= 4:
+        raise NameTooShortError("Name must be more than 4 characters")
+
+    if '@' not in email:
+        raise MustContainAtSymbolError("Email must contain @")
+
+    if email.split('.')[-1] not in ['com', 'bg', 'org', 'net']:
+        raise InvalidDomainError("Domain must be one of the following: .com, .bg, .org, .net")
