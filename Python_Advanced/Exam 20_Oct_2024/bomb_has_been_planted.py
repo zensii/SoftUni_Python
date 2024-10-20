9c27b6f0df27af8601fb3f474166bbdcf9b9a3bf
svetl <svetlin.sii@gmail.com>
2024-10-20 17:20:36 +0300

    added a solution

--- Python_Advanced/Exam 20_Oct_2024/bomb_has_been_planted.py
+++ Python_Advanced/Exam 20_Oct_2024/bomb_has_been_planted.py
@@ -1,6 +1,6 @@
-def check_valid_move(next_position: list[int]) -> bool:
+def check_valid_move(next_position: list[int], n, m) -> bool:
     next_row, next_col = next_position
-    if 0 <= next_row < n and 0 <= next_row < m:
+    if 0 <= next_row < n and 0 <= next_col < m:
         return True
     return False
 
@@ -8,8 +8,8 @@ def check_valid_move(next_position: list[int]) -> bool:
 n, m = list(map(int, input().split(', ')))
 bomb_location = None
 ct_location = None
-ct_dead = False
-bomb_defused = False
+bomb_exploded = False
+time_for_success = 0
 field = []
 TIME = 16
 directions = {'up':(-1, 0), 'right': (0, 1), 'down': (1, 0), 'left': (0, -1)}
@@ -18,42 +18,49 @@ for row in range(n):
     row_data = list(input())
     if 'B' in row_data:
         bomb_location = [row, row_data.index('B')]
-    elif 'C' in row_data:
+    if 'C' in row_data:
         ct_location = [row, row_data.index('C')]
     field.append(row_data)
+
 
 while TIME > 0:
 
     command = input()
-    TIME -= 1
 
     if command == 'defuse':
         if field[ct_location[0]][ct_location[1]] == 'B':
             if TIME >= 4:
                 field[ct_location[0]][ct_location[1]] = 'D'
-                bomb_defused = True
+                print("Counter-terrorist wins!")
+                print(f"Bomb has been defused: {TIME - 4} second/s remaining.")
                 break
+
             else:
                 field[ct_location[0]][ct_location[1]] = 'X'
-                ct_dead = True
+                time_for_success = 4 - TIME
+                bomb_exploded = True
                 break
         else:
             TIME -= 2
+            if TIME <= 0:
+                break
     else:
+        TIME -= 1
         direction = command
         next_pos = list(map(sum, zip(ct_location, directions[direction])))
-        if check_valid_move(next_pos):
+        if check_valid_move(next_pos, n, m):
             row, col = next_pos
             if field[row][col] == 'T':
                 field[row][col] = '*'
-                ct_dead = True
+                print("Terrorists win!")
                 break
-            elif field[row][col] == 'B':
-                continue
 
             ct_location = next_pos
 
-#TODO
 
+if bomb_exploded or TIME <= 0:
+    print("Terrorists win!")
+    print("Bomb was not defused successfully!")
+    print(f"Time needed: {time_for_success} second/s.")
 
-
+[print(''.join(row)) for row in field]
