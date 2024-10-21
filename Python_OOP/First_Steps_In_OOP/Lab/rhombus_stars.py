4b1071b1d98885c055238454e3540ba7e9ea4b38
svetl <svetlin.sii@gmail.com>
2024-10-21 09:50:54 +0300

    added a solution

---
+++ Python_OOP/First_Steps_In_OOP/Lab/rhombus_stars.py
@@ -0,0 +1,14 @@
+def print_line(spaces, stars):
+    return f"{spaces * ' '}{stars * '* '}"
+
+def print_rhombus(n):
+    for i in range(1, n+1):
+        spaces = n - i
+        stars = i
+        print(print_line(spaces, stars))
+    for i in range(n-1, -1, -1):
+        spaces = n - i
+        stars = i
+        print(print_line(spaces, stars))
+
+print_rhombus(6)
