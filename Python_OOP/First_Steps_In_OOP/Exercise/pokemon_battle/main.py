d02337ac1c4768e2bf69dec6c31985ad4303fc5c
svetl <svetlin.sii@gmail.com>
2024-10-21 16:13:16 +0300

    added a solution

---
+++ Python_OOP/First_Steps_In_OOP/Exercise/pokemon_battle/main.py
@@ -0,0 +1,14 @@
+from Python_OOP.First_Steps_In_OOP.Exercise.pokemon_battle.pokemon import Pokemon
+from Python_OOP.First_Steps_In_OOP.Exercise.pokemon_battle.trainer import Trainer
+
+pokemon = Pokemon("Pikachu", 90)
+print(pokemon.pokemon_details())
+trainer = Trainer("Ash")
+print(trainer.add_pokemon(pokemon))
+second_pokemon = Pokemon("Charizard", 110)
+print(trainer.add_pokemon(second_pokemon))
+print(trainer.add_pokemon(second_pokemon))
+print(trainer.release_pokemon("Pikachu"))
+print(trainer.release_pokemon("Pikachu"))
+print(trainer.trainer_data())
+
