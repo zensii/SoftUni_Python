d07927a89f9551cda312f631d6957198e9bc210e
svetl <svetlin.sii@gmail.com>
2024-10-23 00:51:06 +0300

    added a solution

---
+++ Python_OOP/Classes_Objects/Exercise/project_07/album.py
@@ -0,0 +1,51 @@
+from typing import List, Union
+from project_07.song import Song
+
+
+class Album:
+    def __init__(self, name: str, *args: Union[List[Song] | Song]):
+        self.name = name
+        self.published = False
+        self.songs = []
+        for arg in args:
+            if isinstance(arg, list):
+                self.songs.extend(arg)
+            else:
+                self.songs.append(arg)
+
+    def add_song(self, song: Song):
+        if song in self.songs:
+            return f"Song is already in the album."
+        if song.single:
+            return f"Cannot add {song.name}. It's a single"
+        if self.published:
+            return f"Cannot add songs. Album is published."
+        self.songs.append(song)
+        return f"Song {song.name} has been added to the album {self.name}."
+
+    def remove_song(self, song_name: str):
+        for song in self.songs:
+            if song.name == song_name:
+                if self.published:
+                    return f"Cannot remove songs. Album is published."
+                self.songs.remove(song)
+                return f"Removed song {song_name} from album {self.name}."
+        return f"Song is not in the album."
+
+    def publish(self):
+        if not self.published:
+            self.published = True
+            return f"Album {self.name} has been published."
+        return f"Album {self.name} is already published."
+
+    def details(self):
+        return f"Album {self.name}\n" + '\n'.join([f"== {cur_song.get_info()}" for cur_song in self.songs])
+
+if __name__ == '__main__':
+    song = Song("Running in the 90s", 3.45, False)
+    print(song.get_info())
+    album = Album("Initial D", song)
+    second_song = Song("Around the World", 2.34, False)
+    print(album.add_song(second_song))
+    print(album.details())
+    print(album.publish())
