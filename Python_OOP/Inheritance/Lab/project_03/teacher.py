77d211c4d6efeb99ce8017d240169e3518d709f4
svetl <svetlin.sii@gmail.com>
2024-10-23 23:23:52 +0300

    added a solution

--- Python_OOP/Inheritance/Lab/project_03/teacher.py
+++ Python_OOP/Inheritance/Lab/project_03/teacher.py
@@ -1,5 +1,5 @@
-from project.employee import Employee
-from project.person import Person
+from project_03.employee import Employee
+from project_03.person import Person
 
 
 class Teacher(Person,Employee):
