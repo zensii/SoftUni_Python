71ffbb90858a25ae8874868fbe598e5fd2702eec
svetl <svetlin.sii@gmail.com>
2024-10-24 09:35:37 +0300

    added a solution

---
+++ Python_OOP/Inheritance/Lab/project_05/cat.py
@@ -0,0 +1,6 @@
+from project_05.animal import Animal
+
+
+class Cat(Animal):
+    def meow(self):
+        return 'meowing...'
---
+++ Python_OOP/Inheritance/Lab/project_05/dog.py
@@ -0,0 +1,6 @@
+from project_05.animal import Animal
+
+
+class Dog(Animal):
+    def bark(self):
+        return 'barking...'
