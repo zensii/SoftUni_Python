056ef88a63efcbd76985e746b6cd4d61b921e69a
svetl <svetlin.sii@gmail.com>
2024-10-25 09:55:42 +0300

    added a solution

---
+++ Python_OOP/Inheritance/Exercise/project_03/dark_wizard.py
@@ -0,0 +1,6 @@
+from project_03.wizard import Wizard
+
+
+class DarkWizard(Wizard):
+    def __init__(self, username: str, level: int):
+        super().__init__(username, level)
---
+++ Python_OOP/Inheritance/Exercise/project_03/wizard.py
@@ -0,0 +1,6 @@
+from project_03.hero import Hero
+
+
+class Wizard(Hero):
+    def __init__(self, username: str, level: int):
+        super().__init__(username, level)
