864ce952aa4a90290dee92591c38f5e52f423aa3
svetl <svetlin.sii@gmail.com>
2024-10-29 17:45:18 +0200

    added a solution

---
+++ Python_OOP/Encapsulation/Lab/account.py
@@ -0,0 +1,24 @@
+class Account:
+    def __init__(self, id: int, balance:int, pin:int):
+        self.__id = id
+        self.__pin = pin
+        self.balance = balance
+
+    def get_id(self, pin):
+        if self.__pin == pin:
+            return self.__id
+        return f"Wrong pin"
+
+    def change_pin(self, old_pin, new_pin):
+        if self.__pin == old_pin:
+            self.__pin = new_pin
+            return f"Pin changed"
+        else:
+            return f"Wrong pin"
+
+account = Account(8827312, 100, 3421)
+print(account.get_id(1111))
+print(account.get_id(3421))
+print(account.balance)
+print(account.change_pin(2212, 4321))
+print(account.change_pin(3421, 1234))
