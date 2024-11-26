6b2fb7c0636b9ac17089b383aaf608616706ed53
svetl <svetlin.sii@gmail.com>
2024-11-26 13:18:55 +0200

    added a solution

---
+++ Python_OOP/Decorators/Lab/number_increment.py
@@ -0,0 +1,11 @@
+def number_increment(numbers):
+
+    def increase():
+
+        for index in range(len(numbers)):
+            numbers[index] += 1
+        return numbers
+
+    return increase()
+
+print(number_increment([1, 2, 3]))
