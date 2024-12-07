540d3fdb22f01ce1968183c7b9f9701959ed18de
svetl <svetlin.sii@gmail.com>
2024-12-07 14:27:26 +0200

    added a solution

---
+++ Python_OOP/Decorators/Exercise/type_check.py
@@ -0,0 +1,16 @@
+def type_check(typ):
+    def decorator(func):
+        def wrapper(*args):
+            for arg in args:
+                if type(arg) != typ:
+                    return "Bad Type"
+            return func(*args)
+        return wrapper
+    return decorator
+
+
+@type_check(int)
+def times2(num):
+    return num*2
+print(times2(2))
+print(times2('Not A Number'))
